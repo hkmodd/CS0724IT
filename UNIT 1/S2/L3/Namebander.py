0fc84ec6bf550d4c5ff120aee6f99c36a82715ae
Sebastiano Gelmetti <sebastiano.gelmetti@yahoo.it>
2024-11-13 15:26:10 +0100

    upload Pratica S2/L3

---
+++ UNIT 1/S2/L3/Namebander.py
@@ -0,0 +1,32 @@
+# Saluto iniziale
+print("\nCiao! Benvenuto su Namebander! 🎸")
+
+# Funzione per verificare se l'input della città contiene numeri
+def input_citta(testo):
+    while True:
+        valore = input(testo)
+        if any(char.isdigit() for char in valore):
+            print("Solo io che sono un programma vengo dalla città 00110100 00110010")
+        else:
+            return valore
+
+# Funzione per verificare se l'input del nome dell'animale domestico contiene numeri
+def input_animale(testo):
+    while True:
+        valore = input(testo)
+        if any(char.isdigit() for char in valore):
+            print("Elon Musk sei tu!?")
+        else:
+            return valore
+
+# Richiesta di input all'utente
+citta_origine = input_citta("\nPrima di tutto, dimmi, da quale città vieni? ")
+nome_animale = input_animale("\nPerfetto! Ora, qual è il nome del tuo adorato animale domestico? ")
+
+# Generazione del nome della band
+nome_band = citta_origine + " " + nome_animale
+
+# Stampa del risultato
+print("\nFantastico!")
+print("Il nome della tua band sarà:", nome_band)
+
