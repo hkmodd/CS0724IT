207027ad8e1d520a5ec0b295ebf59f369cab7879
hkmodd <sebastiano.gelmetti@yahoo.it>
2025-01-08 01:15:33 +0100

    up

--- UNIT 2/S8/BUILDWEEK 2/Traccia3.c
+++ UNIT 2/S8/BUILDWEEK 2/Traccia3.c
@@ -1,70 +1,81 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-    int vector[10], i, j, k;
-    int swap_var;
-    int scelta;
+void run_classic_mode();
+void run_broken_mode();
 
-    printf("Seleziona un'opzione:\n");
-    printf("1. Esegui il programma corretto\n");
-    printf("2. Esegui il programma con errore (buffer overflow)\n");
-    printf("Scelta: ");
-    if (scanf("%d", &scelta) != 1 || (scelta != 1 && scelta != 2)) {
+int main() {
+    int choice;
+
+    printf("Benvenuto! Scegli una modalit\u00e0:\n");
+    printf("1: Modalit\u00e0 classica\n");
+    printf("2: Modalit\u00e0 rotta (simula segmentation fault)\n");
+    printf("Inserisci la tua scelta (1 o 2): ");
+
+    if (scanf("%d", &choice) != 1 || (choice != 1 && choice != 2)) {
         printf("Errore: Selezione non valida. Inserire 1 o 2.\n");
         return 1;
     }
 
-    if (scelta == 1) {
-        // Programma corretto
-        printf("Hai selezionato il programma corretto.\n");
-        printf("Inserire 10 numeri interi:\n");
-        for (i = 0; i < 10; i++) {
-            printf("[%d]: ", i + 1);
-            if (scanf("%d", &vector[i]) != 1) {
-                printf("Errore: Inserire un numero intero valido.\n");
-                return 1;
-            }
-        }
-
-        printf("\nIl vettore inserito è:\n");
-        for (i = 0; i < 10; i++) {
-            printf("[%d]: %d\n", i + 1, vector[i]);
-        }
-
-        // Ordinamento Bubble Sort
-        for (j = 0; j < 10 - 1; j++) {
-            for (k = 0; k < 10 - j - 1; k++) {
-                if (vector[k] > vector[k + 1]) {
-                    swap_var = vector[k];
-                    vector[k] = vector[k + 1];
-                    vector[k + 1] = swap_var;
-                }
-            }
-        }
-
-        printf("\nIl vettore ordinato è:\n");
-        for (i = 0; i < 10; i++) {
-            printf("[%d]: %d\n", i + 1, vector[i]);
-        }
-
-    } else if (scelta == 2) {
-        // Programma con errore
-        printf("Hai selezionato il programma con errore (buffer overflow).\n");
-        printf("Inserire 15 numeri interi:\n");
-
-        for (i = 0; i < 15; i++) { // Accede oltre i limiti di vector[10]
-            if (i >= 10) {
-                printf("\n[!] ERRORE CRITICO: BUFFER OVERFLOW RILEVATO! (Scrittura oltre i limiti)\n");
-            }
-
-            // Forza un vero errore di segmentazione
-            printf("[%d]: ", i + 1);
-            scanf("%d", &vector[i]); // Scrittura oltre i limiti di vector[10]
-        }
-
-        printf("Questo messaggio non dovrebbe mai apparire!\n");
+    if (choice == 1) {
+        run_classic_mode();
+    } else if (choice == 2) {
+        run_broken_mode();
     }
 
     return 0;
+}
+
+void run_classic_mode() {
+    int vector[10], i, j, k;
+    int swap_var;
+
+    printf("Inserire 10 numeri interi:\n");
+
+    for (i = 0; i < 10; i++) {
+        printf("[%d]: ", i + 1);
+        scanf("%d", &vector[i]);
+    }
+
+    printf("\nIl vettore inserito \u00e8:\n");
+    for (i = 0; i < 10; i++) {
+        printf("[%d]: %d\n", i + 1, vector[i]);
+    }
+
+    for (j = 0; j < 10 - 1; j++) {
+        for (k = 0; k < 10 - j - 1; k++) {
+            if (vector[k] > vector[k + 1]) {
+                swap_var = vector[k];
+                vector[k] = vector[k + 1];
+                vector[k + 1] = swap_var;
+            }
+        }
+    }
+
+    printf("\nIl vettore ordinato \u00e8:\n");
+    for (i = 0; i < 10; i++) {
+        printf("[%d]: %d\n", i + 1, vector[i]);
+    }
+}
+
+void run_broken_mode() {
+    int vector[10], i;
+    int *ptr = NULL; // Puntatore nullo
+
+    printf("Inserire 10 numeri interi:\n");
+
+    for (i = 0; i < 10; i++) {
+        printf("[%d]: ", i + 1);
+        scanf("%d", &vector[i]);
+    }
+
+    printf("\nIl vettore inserito \u00e8:\n");
+    for (i = 0; i < 10; i++) {
+        printf("[%d]: %d\n", i + 1, vector[i]);
+    }
+
+    printf("\nErrore...\n");
+
+    // Generazione intenzionale del segmentation fault
+    printf("Valore in *ptr: %d\n", *ptr);
 }
